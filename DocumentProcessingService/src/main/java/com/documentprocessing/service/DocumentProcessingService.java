63095107b0253f3f4443d60346d013b40f6e5c6c
Narendra reddy <narendrapallaki@gmail.com>
2024-07-24 12:09:23 +0530

    docement processing reading images and pdf

---
+++ DocumentProcessingService/src/main/java/com/documentprocessing/service/DocumentProcessingService.java
@@ -0,0 +1,133 @@
+package com.documentprocessing.service;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.documentprocessing.entity.Aadhaar;
+import com.documentprocessing.repository.DocumentProcessingRepository;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+@Service
+public class DocumentProcessingService {
+
+	@Autowired
+	private DocumentProcessingRepository documentProcessingRepository;
+
+	public String processAndSaveAadhaarDetails(MultipartFile file) {
+		ITesseract tesseract = new Tesseract();
+		tesseract.setDatapath("tessdata");
+
+		try {
+			File tempFile = File.createTempFile("temp", file.getOriginalFilename());
+			file.transferTo(tempFile);
+			String text = tesseract.doOCR(tempFile);
+			System.out.println("Extracted Aadhaar data: " + text);
+
+			Aadhaar aadhaar = parseTextToAadhaar(text);
+			// documentProcessingRepository.save(aadhaar);
+
+			return "Details saved successfully!";
+		} catch (TesseractException | IOException e) {
+			e.printStackTrace();
+			return "Failed to process and save details";
+		}
+	}
+
+	private Aadhaar parseTextToAadhaar(String text) {
+		String aadhaarNumber = extractPattern(text, "\\b\\d{4} \\d{4} \\d{4}\\b");
+		String gender = extractPattern(text, "\\bMALE\\b|\\bFEMALE\\b");
+		String name = extractPattern(text, " [A-Za-z]+");
+		// String dateOfBirth = extractPattern(text, "\\bDOB[:
+		// ]+\\d{2}/\\d{2}/\\d{4}\\b");
+		String dateOfBirth = extractPattern(text, "\\b(?:DOB|Year of Birth)[: ]+(\\d{2}/\\d{2}/(\\d{4}))\\b");
+
+		if (dateOfBirth != null) {
+			dateOfBirth = dateOfBirth.split(":")[1].trim();
+		}
+		String address = extractPattern(text, "address:.*");
+		if (address != null) {
+			address = address.replace("address:", "").trim();
+		}
+
+		Aadhaar aadhaar = new Aadhaar();
+		aadhaar.setAadhaarNumber(aadhaarNumber);
+		aadhaar.setName(name);
+		aadhaar.setDateOfBirth(dateOfBirth);
+		aadhaar.setGender(gender);
+		aadhaar.setAddress(address);
+		System.out.println("Aadhaar data : " + aadhaar);
+		return aadhaar;
+	}
+
+	private String extractPattern(String text, String patternString) {
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(text);
+		if (matcher.find()) {
+			return matcher.group();
+		}
+		return null;
+	}
+
+	public String readDocumentResolution(MultipartFile file) throws IOException, TesseractException {
+
+		BufferedImage ipimage = ImageIO.read(file.getInputStream());
+
+		double d = ipimage.getRGB(ipimage.getTileWidth() / 2, ipimage.getTileHeight() / 2);
+
+		if (d >= -1.4211511E7 && d < -7254228) {
+			processImg(ipimage, 1.4f, -10f);
+		} else if (d >= -7254228 && d < -2171170) {
+			processImg(ipimage, 1.2f, -5f);
+		} else if (d >= -2171170 && d < -1907998) {
+			processImg(ipimage, 1.1f, -2f);
+		} else if (d >= -1907998 && d < -257) {
+			processImg(ipimage, 1.05f, -1f);
+		} else if (d >= -257 && d < -1) {
+			processImg(ipimage, 1f, -0.5f);
+		} else if (d >= -1 && d < 2) {
+			processImg(ipimage, 1f, -0.35f);
+		}
+
+		return "Document processed successfully";
+	}
+
+	public static void processImg(BufferedImage ipimage, float scaleFactor, float offset)
+			throws IOException, TesseractException {
+
+		BufferedImage opimage = new BufferedImage(1050, 1024, BufferedImage.TYPE_BYTE_GRAY);
+
+		Graphics2D graphic = opimage.createGraphics();
+
+		graphic.drawImage(ipimage, 0, 0, 1050, 1024, null);
+		graphic.dispose();
+
+		// Increase contrast
+		RescaleOp rescale = new RescaleOp(scaleFactor, offset, null);
+		BufferedImage fopimage = rescale.filter(opimage, null);
+
+		// Save the preprocessed image for verification
+		 ImageIO.write(fopimage, "jpg", new File("C:\\Html\\myAadherOpt111.png"));
+
+		// Perform OCR
+		Tesseract tesseract = new Tesseract();
+		// tesseract.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
+		tesseract.setDatapath("tessdata");
+
+		String result = tesseract.doOCR(fopimage);
+		System.out.println("readDocumentAfterResolution :" + result);
+	}
+}
